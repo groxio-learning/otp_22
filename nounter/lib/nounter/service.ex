7e2786d7564335ef88593515710bb7fbd83e02fb
paulo-valim <plvalim@gmail.com>
2022-11-09 21:23:29 +0100

    message

--- nounter/lib/nounter/service.ex
+++ nounter/lib/nounter/service.ex
@@ -1,16 +1,29 @@
 defmodule Nounter.Service do
-  alias Nounter.Counter, as: Counter
+  alias Nounter.Counter
+
+  def start(input) do
+    counter = Counter.new(input)
+    spawn(fn -> loop(counter) end)
+  end
+
+  def loop(counter) do
+    counter
+    |> listen()
+    |> loop()
+  end
+
   def listen(counter) do
     receive do
       :inc ->
         Counter.inc(counter)
+
       :dec ->
         Counter.dec(counter)
+
       {:show, from} ->
         new_state = counter
         send(from, Counter.show(counter))
         new_state
     end
-
   end
 end
